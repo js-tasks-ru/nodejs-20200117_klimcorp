b61dff650fd5fbbfb71c71e09d848329634d37a7
Pavel Klimenko <paul.klimen@gmail.com>
2020-02-02 23:36:54 +0300

    complete 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,20 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._totalChunk = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this._totalChunk += chunk.toString();
+    callback();
   }
 
   _flush(callback) {
+    const lines = this._totalChunk.split(os.EOL);
+    lines.forEach((line) => {
+      this.push(line);
+    });
+    callback();
   }
 }
 
