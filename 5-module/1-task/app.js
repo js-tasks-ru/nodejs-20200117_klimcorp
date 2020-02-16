6ba7cce3a33c1d5c2181029237da22f8748f8560
Pavel Klimenko <paul.klimen@gmail.com>
2020-02-16 16:27:36 +0300

    resolve 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -7,10 +7,30 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  const addSubscriber = new Promise((res) => {
+    subscribers.push(res);
+  });
+
+  ctx.body = await addSubscriber;
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post('/publish', async (ctx) => {
+  const message = ctx.request.body.message;
+
+  if (!message) {
+    ctx.throw(400, 'message is empty');
+  }
+
+  subscribers.forEach((resolve) => {
+    resolve(message);
+  });
+
+  subscribers = [];
+
+  ctx.body = 'ok';
 });
 
 app.use(router.routes());
